9bf8f39a8ad1fceab0d440632f57d1d388f3161c
Isaac Oluwafemi Ogunniyi <isaacogunniyi1@gmail.com>
2022-09-29 21:18:06 +0000

    Update romanNumeralConverter.js
    Adding more comments to explain the code and editing the variable names to better reflect what has been assigned to them.

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,13 +1,17 @@
 /*This project consist of a JavaScript code that converts an input number into its equivalent roman numeral.
 It is important to note that this works for only numbers up to 99999 but the functionality can be easily extended*/
 function convertToRoman(num) {
-   let answ=[]
-   let fLetters=["I","X","C","M"];
+   
+   //Defining variables and doing some initialization
+   let answ=[] // container array for the letters that will make up the equivalent roman numeral (letters will be stored in order)
+   let fLetters=["I","X","C","M"]; //various letters that represent
    let mLetters=["V","L","D"];
    let ans=[];
    let q=1;
-   let numModStr=num.toString();
-   let strToArr=[...numModStr];
+   let stringifiedNum=num.toString(); // Converting the input argument (num) into a string so as to allow easy handling of the individual digits that make up 'num'
+   let arrOfDigits=[...stringifiedNum]; // upacking the stringified 'num' into an array 
+   
+   /* Defining the various nested functions that will be necessary for the code algorithm*/
    function lessthan(n,times){
      while (times>0){
        ans.push(n)
@@ -42,8 +46,8 @@ function convertToRoman(num) {
         return ans;
       }
    }
-   for (let i=strToArr.length-1;i>=0;i--){
-     let res=convSingDig(parseInt(strToArr[i]),q++);
+   for (let i=arrOfDigits.length-1;i>=0;i--){ //iterating through the digits of 'num' from the last digit through to the first digit
+     let res=convSingDig(parseInt(arrOfDigits[i]),q++);
      ans=[];
      answ.unshift(...res);
    }
