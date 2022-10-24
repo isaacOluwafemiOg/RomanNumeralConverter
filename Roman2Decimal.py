5f71547eb569d5e21b45309cb412385e0bd02f8e
Isaac Oluwafemi Ogunniyi <isaacogunniyi1@gmail.com>
2022-10-24 08:03:13 +0000

    Update Roman2Decimal.py
    Refactored code to make it concise

--- Roman2Decimal.py
+++ Roman2Decimal.py
@@ -9,7 +9,7 @@ def romanToInt(s):
    res=0
    sym_val={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
    i=0
-   while i <=len(s)-1:
+   while i <=len(s):
     
     if i==len(s)-1:
       
@@ -21,15 +21,7 @@ def romanToInt(s):
     elif sym_val[s[i]]<sym_val[s[i+1]]:
       v=sym_val[s[i+1]]-sym_val[s[i]]
       res+=v
-                   
-      if i+2 < len(s) - 1:
-        
-        i+=2
-        continue
-      elif i+2>len(s)-1:
-        return res
-      elif i+2==len(s)-1:
-        res+=sym_val[s[i+2]]
-        return  res
+      i+=2
+      continue
      i+=1
    return res
